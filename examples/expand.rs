af28b05ab34762c8bd28bb0a7dd68a511b1ecb18
Vitaly _Vi Shukela <vi0oss@gmail.com>
2022-03-24 21:37:34 +0300

    Refactor + CLI tool

---
+++ examples/expand.rs
@@ -0,0 +1,17 @@
+use std::{ffi::OsString};
+
+use quote::ToTokens;
+
+fn main() -> Result<(), syn_file_expand::Error> {
+    let args = Vec::<OsString>::from_iter(std::env::args_os());
+    if args.len() != 2 {
+        println!("Usage: expand <Rust source file>");
+        println!("Reads rust source file, including referred modules and expands them into a single source with all modules inline");
+        println!("Conditional paths like #[cfg_attr(feature=\"qqq\"),path=...) are resolved using");
+        println!("environment variables like SYN_FILE_EXPAND_FEATURE_QQQ=1");
+        std::process::exit(1);
+    }
+    let source = syn_file_expand::read_full_crate_source_code(&args[1], |_|false)?;
+    println!("{}", source.into_token_stream());
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use proc_macro2::{TokenStream, TokenTree};
 use std::path::PathBuf;
 use thiserror::Error;
 
@@ -100,176 +99,58 @@ pub fn expand_modules_into_inline_modules<R: Resolver>(
     content: &mut syn::File,
     resolver: &mut R,
 ) -> Result<(), Error> {
-    expand_impl(&mut content.items, resolver, Vector::new(), Vector::new())?;
+    let dirs = Vector::new();
+    expand_impl::expand_impl(&mut content.items, resolver, Vector::new(), dirs)?;
     Ok(())
 }
 
-fn expand_impl<R: Resolver>(
-    content: &mut Vec<syn::Item>,
-    resolver: &mut R,
-    modules_stack: Vector<syn::Ident>,
-    dirs: Vector<PathBuf>,
-) -> Result<(), Error> {
-    for item in content {
-        let (item_mod, semicolon_after_module_declaration) = match item {
-            syn::Item::Mod(ref mut item_mod) => {
-                match (&item_mod.content, item_mod.semi) {
-                    (None, None) => panic!("A module without both `{{}}` and `;`?"),
-                    (Some(_), Some(_)) => panic!("A module with both `{{}}` and `;`?"),
-                    (Some(_), None) => continue, // inline module
-                    (None, Some(semi)) => (item_mod, semi),
-                }
-            }
-            _ => continue,
-        };
+/// Easy function to load full crate source code from the filesystem.
+///
+/// If `#[cfg_attr(some_meta,path=...)]` is encountered while reading modules, your callback
+/// predicate `cfg_attr_path_handler` is called with `some_meta` and should decide whether to
+/// follow this cfg_attr or not
+/// 
+/// Just specify `|_|false` there if you are confused.
+/// 
+/// Security: Note that content of file being loaded may point to arbitrary file, including using absolute paths.
+/// Use IO-less `expand_modules_into_inline_modules` function if you want to control what is allowed to be read.
+pub fn read_full_crate_source_code(
+    path: impl AsRef<std::path::Path>,
+    mut cfg_attr_path_handler: impl FnMut(syn::Meta) -> bool,
+) -> Result<syn::File, Error> {
+    let path = path.as_ref();
+    let root_source = std::fs::read_to_string(path).map_err(|e| Error::FailedToOpenFile {
+        path: path.to_owned(),
+        e,
+    })?;
+    let mut root_source: syn::File =
+        syn::parse_file(&root_source).map_err(|e| Error::SynParseError {
+            module: syn::Path {
+                leading_colon: None,
+                segments: syn::punctuated::Punctuated::new(),
+            },
+            e,
+        })?;
+    
+    let parent_dir = path.parent();
 
-        let id = item_mod.ident.clone();
-
-        let mut inner_stack = modules_stack.clone();
-        inner_stack.push_back(id.clone());
-
-        let chunk = PathBuf::from(format!("{}", id));
-        let chunk_rs = PathBuf::from(format!("{}.rs", id));
-
-        let mut dirs = dirs.clone();
-
-        let len_hint = dirs.len()
-            + std::convert::identity::<usize>(dirs.iter().map(|x| x.as_os_str().len()).sum())
-            + 8
-            + chunk.as_os_str().len();
-
-        let mut module_file_nomod = PathBuf::with_capacity(len_hint);
-        let mut module_file_mod = PathBuf::with_capacity(len_hint);
-
-        for x in &dirs {
-            module_file_mod.push(x);
-            module_file_nomod.push(x);
-        }
-        module_file_mod.push(&chunk);
-        module_file_mod.push("mod.rs");
-        module_file_nomod.push(&chunk_rs);
-
-        let mod_syn_path = syn::Path {
-            leading_colon: None,
-            segments: syn::punctuated::Punctuated::from_iter(inner_stack.iter().map(|x| {
-                syn::PathSegment {
-                    ident: x.clone(),
-                    arguments: syn::PathArguments::None,
-                }
-            })),
-        };
-
-        let mut attrs = Vec::with_capacity(item_mod.attrs.len());
-
-        let mut inner: Option<Option<syn::File>> = None;
-
-        let mut dirs_candidate = Vector::new();
-
-        let mut path_attrs: Vec<(Vec<TokenTree>, Option<TokenStream>)> = Vec::new();
-        attrs::extract_path_and_cfg_path_attrs(
-            &item_mod.attrs,
-            &mut path_attrs,
-            &mod_syn_path,
-            &mut attrs,
-        )?;
-
-        for (tt, cfg) in path_attrs {
-            if cfg.is_none() && inner.is_some() {
-                return Err(Error::MultipleExplicitPathsSpecifiedForOneModule {
-                    module: mod_syn_path,
-                });
-            }
-
-            let explicit_path = attrs::extract_path_from_attr(tt, &mod_syn_path)?;
-
-            let mut module_file_explicit = PathBuf::with_capacity(len_hint);
-
-            for x in &dirs {
-                module_file_explicit.push(x);
-            }
-            module_file_explicit.push(explicit_path);
-
-            dirs_candidate = Vector::new();
-            if let Some(parent) = module_file_explicit.parent() {
-                dirs_candidate.push_back(parent.to_owned());
-            }
-            dirs_candidate.push_back(PathBuf::from(chunk.clone()));
-
-            if let Some(cfg) = cfg {
-                let cfg: syn::Meta = syn::parse2(cfg).map_err(|e| Error::SynParseError {
-                    module: mod_syn_path.clone(),
-                    e,
-                })?;
-                if let Some(x) =
-                    resolver.resolve(mod_syn_path.clone(), module_file_explicit, Some(cfg))?
-                {
-                    if inner.is_some() {
-                        return Err(Error::MultipleExplicitPathsSpecifiedForOneModule {
-                            module: mod_syn_path,
-                        });
-                    }
-                    inner = Some(Some(x));
-                }
-            } else {
-                inner = Some(resolver.resolve(mod_syn_path.clone(), module_file_explicit, None)?);
+    expand_modules_into_inline_modules(&mut root_source, &mut |module_name: syn::Path, file_path: PathBuf, cfg| {
+        if let Some(cfg) = cfg {
+            if ! cfg_attr_path_handler(cfg) {
+                return Ok(None);
             }
         }
-
-        let inner = if let Some(i) = inner {
-            dirs = dirs_candidate;
-            i
+        let path = if let Some(parent_dir) = parent_dir {
+            parent_dir.join(&file_path)
         } else {
-            dirs.push_back(PathBuf::from(chunk));
-            let inner_nomod = resolver.resolve(mod_syn_path.clone(), module_file_nomod, None);
-            match inner_nomod {
-                Ok(_) => (),
-                Err(Error::FailedToOpenFile { .. }) => (),
-                Err(e) => return Err(e),
-            }
-            let inner_mod = resolver.resolve(mod_syn_path.clone(), module_file_mod.clone(), None);
-            match inner_mod {
-                Ok(_) => (),
-                Err(Error::FailedToOpenFile { .. }) => (),
-                Err(e) => return Err(e),
-            }
-            match (inner_nomod, inner_mod) {
-                (Ok(Some(_)), Ok(Some(_))) => {
-                    return Err(Error::BothModRsAndNameRsPresent {
-                        module: mod_syn_path,
-                    })
-                }
-                (Ok(None), Ok(None)) => None,
-                (Ok(Some(x)), _) => Some(x),
-                (_, Ok(Some(x))) => Some(x),
-                (Err(e), _) => return Err(e),
-                (_, Err(e)) => return Err(e),
-            }
+            file_path
         };
-
-        if let Some(inner) = inner {
-            let mut items = inner.items;
-
-            for attr in inner.attrs {
-                attrs.push(attr);
-            }
-
-            expand_impl(&mut items, resolver, inner_stack, dirs)?;
-
-            let new_mod = syn::ItemMod {
-                attrs,
-                vis: item_mod.vis.clone(),
-                mod_token: item_mod.mod_token,
-                ident: id,
-                content: Some((
-                    syn::token::Brace(semicolon_after_module_declaration.span),
-                    items,
-                )),
-                semi: None,
-            };
-            *item = syn::Item::Mod(new_mod);
-        }
-    }
-    Ok(())
+        let module_source = std::fs::read_to_string(&path).map_err(|e|Error::FailedToOpenFile { path: path.clone(), e})?;
+        let module_source : syn::File = syn::parse_file(&module_source).map_err(|e|Error::SynParseError { module:module_name, e})?;
+        Ok(Some(module_source))
+    })?;
+    Ok(root_source)
 }
 
 mod attrs;
+mod expand_impl;
